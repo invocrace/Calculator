53c718394942aecf127a0d18a4686e04321261ef
moi0892 <moi0892@hotmail.fr>
2021-01-03 18:34:14 +0100

    utilisation de calculator dans la class MultipleOperations + création de la class de test.

--- src/main/java/com/calculator/laFameuse/endPoint/MultipleOperations.java
+++ src/main/java/com/calculator/laFameuse/endPoint/MultipleOperations.java
@@ -4,7 +4,9 @@ import com.calculator.laFameuse.operation.withTwoArguments.OperationTwoArguments
 import com.calculator.laFameuse.operations.Operation;
 
 public class MultipleOperations {
-Calculator calculator = new Calculator();
+
+private final Calculator calculator = new Calculator();
+
     public double multipleOperations(OperationTwoArguments operationMother, Operation operation1, Operation operation2){
         return operationMother.operation( calculator.resOperation(operation1),calculator.resOperation(operation2));
     }
--- src/test/java/com/calculator/laFameuse/operation/withTwoArguments/MultipleOperationsTest.java
+++ src/test/java/com/calculator/laFameuse/operation/withTwoArguments/MultipleOperationsTest.java
@@ -3,6 +3,8 @@ package com.calculator.laFameuse.operation.withTwoArguments;
 import com.calculator.laFameuse.endPoint.MultipleOperations;
 import com.calculator.laFameuse.operations.Operation;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,10 +12,27 @@ public class MultipleOperationsTest {
 
     @Test
     public void shouldChainSomeOperations(){
-        Operation operation = new Addition();
-        Operation operation2 = new Soustraction();
         OperationTwoArguments operationMother = new Addition();
         MultipleOperations multipleOperations = new MultipleOperations();
+
+        Operation operation = Mockito.mock(Operation.class);
+        Mockito.doReturn(4.0).when(operation).operation();
+
+        Operation operation2 = Mockito.mock(Operation.class);
+        Mockito.doReturn(4.0).when(operation2).operation();
+
+        assertEquals(8.0,multipleOperations.multipleOperations(operationMother,operation,operation2),0.00001);
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
